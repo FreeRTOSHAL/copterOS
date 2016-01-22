d2540c02da8f242ee3e0775502921cce9b72d4fa
Werner2005 <webmaster@andy89.org>
2016-01-22 16:36:44 +0100

    move to new repo

--- tools/autocopt-tools/README.md
+++
@@ -1,6 +0,0 @@
-Roadsite Detection
-==================
-This is a projekt of Torben Grünewald and Andreas Werner. 
-Call the Programm with: 
-
-./src/video
--- tools/autocopt-tools/src/autocopt_prv.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUTOCOPT_PRV_H_
-#define AUTOCOPT_PRV_H_
-#include <signal.h>
-struct autocopt {
-	char *dev;
-	int fd;
-	struct sigaction oldact;
-};
-#endif
